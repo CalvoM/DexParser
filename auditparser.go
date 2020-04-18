2268fc851c12ea8232f0a9afd6a570c22136953b
CALVIN MWADIME <calvin@novek.io>
2020-04-18 23:48:10 +0300

    Clean code

--- auditparser.go
+++ auditparser.go
@@ -5,47 +5,58 @@ import (
 	"strings"
 )
 
-type Product struct{
+//Product :model
+type Product struct {
 	Number string `json:"number"`
-	Price string `json:"price"`
-	Vends string `json:"vends"`
+	Price  string `json:"price"`
+	Vends  string `json:"vends"`
 }
+
+//Event :model
 type Event struct {
-	Type string `json:"type"`
+	Type     string `json:"type"`
 	Duration uint64 `json:"duration"`
 }
-func StreamLine(fileID os.File,index int)(string, int){
-	var buffer = make([]byte,0)
-	var char = make([]byte,1)
-	var count =0
-	_,err :=fileID.ReadAt(char,int64(index))
-	checkError(err,"(StreamLine) Error Reading")
-	buffer = append(buffer,char[0])
-	for char[0] !=10 {
+
+//StreamLine :get a line from file
+func StreamLine(fileID os.File, index int) (string, int) {
+	var buffer = make([]byte, 0)
+	var char = make([]byte, 1)
+	var count = 0
+	_, err := fileID.ReadAt(char, int64(index))
+	checkError(err, "(StreamLine) Error Reading")
+	buffer = append(buffer, char[0])
+	for char[0] != 10 {
 		index++
 		count++
-		_, err = fileID.ReadAt(char,int64(index))
-		checkError(err,"(StreamLine) Loop Error Reading")
-		buffer = append(buffer,char[0])
+		_, err = fileID.ReadAt(char, int64(index))
+		checkError(err, "(StreamLine) Loop Error Reading")
+		buffer = append(buffer, char[0])
 	}
 	count++
-	return string(buffer),count
+	return string(buffer), count
 }
-func GetFirstDataElement(line string)string{
- 	dataElement := strings.Split(line,"*")[0]
- 	return dataElement
+
+//GetFirstDataElement get fields from line
+func GetFirstDataElement(line string) string {
+	dataElement := strings.Split(line, "*")[0]
+	return dataElement
 }
-func IsHeader(header string,headers []string)(isPresent bool){
+
+//IsHeader :check if header is restricted
+func IsHeader(header string, headers []string) (isPresent bool) {
 	isPresent = false
 	for _, h := range headers {
-		if h==header{
+		if h == header {
 			isPresent = true
 			break
 		}
 	}
 	return
 }
-func ParseProduct(line string) (product Product){
+
+//ParseProduct :parse the product line
+func ParseProduct(line string) (product Product) {
 	var productDetails = strings.Split(line, "*")
 	product.Number = productDetails[2]
 	product.Price = productDetails[3]
--- main.go
+++ main.go
@@ -1,32 +1,34 @@
 package main
+
 import (
 	"fmt"
 	"os"
 )
-func checkError(err error,msg string){
+
+func checkError(err error, msg string) {
 	if err != nil {
 		fmt.Println(msg)
 	}
 }
- func main(){
- 	var products []Product;
- 	var headers = []string{"DXS","ST","G85","SE","DXE"}
- 	fileID,err := os.Open("./source/ddcmp.txt")
- 	checkError(err,"(Main) Opening File")
- 	fmt.Println(headers)
- 	var index = 0
- 	for count:=0;count<147;count++{
- 		line,byteCount:=StreamLine(*fileID, index)
- 		dataElement := GetFirstDataElement(line)
-		if IsHeader(dataElement,headers)!=true{
-			if dataElement == "LA1"{
-				products = append(products,ParseProduct(line))
+func main() {
+	var products []Product
+	var headers = []string{"DXS", "ST", "G85", "SE", "DXE"}
+	fileID, err := os.Open("./source/ddcmp.txt")
+	checkError(err, "(Main) Opening File")
+	fmt.Println(headers)
+	var index = 0
+	for count := 0; count < 147; count++ {
+		line, byteCount := StreamLine(*fileID, index)
+		dataElement := GetFirstDataElement(line)
+		if IsHeader(dataElement, headers) != true {
+			if dataElement == "LA1" {
+				products = append(products, ParseProduct(line))
 				fmt.Println(products)
 			}
 			if dataElement == "EA1" {
 				fmt.Println(line)
 			}
 		}
- 		index += byteCount
+		index += byteCount
 	}
- }
+}
